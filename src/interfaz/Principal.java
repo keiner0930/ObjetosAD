c732774c27a271200442af25f47648f8c0b3326a
“keiner0930” <keiner.arrieta0930@hotmail.com>
2016-09-02 10:33:12 -0500

    Validaciones :)

--- src/interfaz/Principal.java
+++ src/interfaz/Principal.java
@@ -3,9 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package interfaz;
+
 import clases.Persona;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author karrieta9
@@ -15,8 +18,8 @@ public class Principal extends javax.swing.JFrame {
     /**
      * Creates new form Principal
      */
-    Persona v[]=new Persona[5];
-    int cont=0;
+    ArrayList<Persona> v = new ArrayList();
+
     public Principal() {
         initComponents();
     }
@@ -84,9 +87,19 @@ public class Principal extends javax.swing.JFrame {
         jPanel3.add(cmdGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, -1, -1));
 
         cmdMostrar.setText("Mostrar");
+        cmdMostrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdMostrarActionPerformed(evt);
+            }
+        });
         jPanel3.add(cmdMostrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 70, 70, -1));
 
         cmdLimpiar.setText("Limpiar");
+        cmdLimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmdLimpiarActionPerformed(evt);
+            }
+        });
         jPanel3.add(cmdLimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 110, 70, -1));
 
         jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 60, 130, 160));
@@ -118,18 +131,55 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cmdGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdGuardarActionPerformed
-     Persona p;
-     long identificacion;
-     String primer_nombre,primer_apellido;
-     
-     identificacion= Long.parseLong(txtIdentificacion.getText());
-     primer_nombre= txtPrimerNombre.getText();
-     primer_apellido= txtPrimerApellido.getText();
-     
-     p= new Persona(identificacion,primer_nombre,primer_apellido);
-     v[cont]=p;
-     cont++;
+        Persona p;
+        long identificacion;
+        String primer_nombre, primer_apellido;
+
+        identificacion = Long.parseLong(txtIdentificacion.getText());
+        primer_nombre = txtPrimerNombre.getText();
+        primer_apellido = txtPrimerApellido.getText();
+
+        p = new Persona(identificacion, primer_nombre, primer_apellido);
+        v.add(p);
+        JOptionPane.showMessageDialog(this, "Personas Agregada Exitosamente");
+        txtIdentificacion.setText("");
+        txtPrimerNombre.setText("");
+        txtPrimerApellido.setText("");
+        txtIdentificacion.requestFocusInWindow();
     }//GEN-LAST:event_cmdGuardarActionPerformed
+
+    private void cmdMostrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdMostrarActionPerformed
+        String aux;
+        txtResultado.setText("");
+        
+        if(v.isEmpty()){
+        txtResultado.setText("No hay Personas que Mostrar");
+        }
+        else{
+        for (int i = 0; i < v.size(); i++) {
+            aux = "Persona No. " + (i + 1) + "\n"
+                    + "Identificacion: " + v.get(i).getIdentificacion() + "\n"
+                    + "Primer Nombre: " + v.get(i).getPrimer_nombre() + "\n"
+                    + "Primer Apellido: " + v.get(i).getPrimer_apellido() + "\n\n";
+
+            txtResultado.append(aux);
+        }
+        }
+
+    }//GEN-LAST:event_cmdMostrarActionPerformed
+
+    private void cmdLimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdLimpiarActionPerformed
+        int op;
+        op = JOptionPane.showConfirmDialog(this, "Seguro que deseas eliminar las personas?", "Pregunta", JOptionPane.YES_NO_OPTION);
+        if (op == JOptionPane.YES_OPTION) {
+            v.clear();
+            txtIdentificacion.setText("");
+            txtPrimerNombre.setText("");
+            txtPrimerApellido.setText("");
+            txtResultado.setText("");
+            txtIdentificacion.requestFocusInWindow();
+        }
+    }//GEN-LAST:event_cmdLimpiarActionPerformed
 
     /**
      * @param args the command line arguments
